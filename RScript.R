16f9665f89dd36281af8ad679309d2e967e8a174
Mohsin Shah <mohsin.shah@outlook.com>
2016-01-25 00:36:07 +0000

    ggmap sample loaded

--- RScript.R
+++ RScript.R
@@ -1,1 +1,23 @@
 paste("There are","5","books")
+x = c(2,5,1,0)
+mean(x)
+x[2] = NA
+is.na(x)
+
+x = Sys.Date()
+x
+x - 1000
+
+# to convert character to date use as.Date
+
+require(ggmap)
+
+mapImageData3 <- get_map(location = "UK",
+                         color = "color",
+                        source = "google",
+                      maptype = "roadmap",
+)
+                         
+
+ggmap(mapImageData3, 
+      extent = "auto")
