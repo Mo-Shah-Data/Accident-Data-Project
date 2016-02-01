cb477418bb99fb369c7e476b52c9522bf517b239
Mohsin Shah <mohsin.shah@outlook.com>
2016-02-01 22:28:05 +0000

    Cleaned up Shiny files

--- ShinyPractice.R
+++ ShinyPractice.R
@@ -1,6 +1,10 @@
 library(shiny)
 library(leaflet)
 
+#To remove a file use the code below
+fn <- "global.r"
+if (file.exists(fn)) file.remove(fn)
+
 r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
 
